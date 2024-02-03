9e79049e1edd317c30b4453bc283f6f710b981db
Dennis Ramilo <lesdeane99@gmail.com>
2024-02-02 23:10:27 -0500

    succeeded communicating with openai api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import { useState } from "react";
+
 const App = () => {
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+
+  const getMessages = async () => {
+    console.log("here");
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(message);
+
   return (
     <div className="app">
       {/* this is for the sidebar */}
@@ -19,8 +50,10 @@ const App = () => {
         <ul className="feed"></ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
-            <div className="submit-symbol">➤</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div className="submit-symbol" onClick={getMessages}>
+              ➤
+            </div>
           </div>
           <p className="info">
             LesGPT v1.0 AI chatbot can make mistakes. Consider checking
