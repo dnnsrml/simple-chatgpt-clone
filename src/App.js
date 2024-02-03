7ea8e33affbe89ccc9a2ae35d377b423bfa27c6a
Dennis Ramilo <lesdeane99@gmail.com>
2024-02-02 19:26:00 -0500

    finished css styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      {/* this is for the sidebar */}
+      <section className="side-bar">
+        <button className="new-chat-button">+ New Chat</button>
+        <ul className="history">
+          <li>There is a movie about me</li>
+          <li>Let's jump in the pool!</li>
+        </ul>
+        <nav className="main-nav">
+          <p>Made by LesDeane</p>
+        </nav>
+      </section>
+
+      {/* this is for the main body */}
+      <section className="main">
+        <h1 className="app-name">LesGPT v1.0</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div className="submit-symbol">➤</div>
+          </div>
+          <p className="info">
+            LesGPT v1.0 AI chatbot can make mistakes. Consider checking
+            important information using other sources.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default App;
